fa8f8dc503fca1a9f9803f1416f4f62dd5ccd7d8
seraphoffun <seraphoffun@gmail.com>
2024-10-15 17:11:22 +0400

    craeting board

--- BatleShipfinal/BatleShipfinal.cpp
+++ BatleShipfinal/BatleShipfinal.cpp
@@ -26,6 +26,184 @@ void clearConsole() {
 	system("clear");
 #endif
 }
+bool findelement(int num, int op, int(*arr)[10]) {
+	int l = 0;
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			if (arr[i][j] % 10 == num)
+			{
+				l++;
+			}
+
+		}
+
+	}
+	if (op * num == l)
+		return true;
+	else
+		return false;
+}
+void shipcordinates(int& x, int& y, int go, int ship, int& d, int(*arr)[10]) {
+
+	int tx = x;
+	int ty = y;
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			if (arr[i][j] < 0)
+			{
+				arr[i][j] = 0;
+			}
+
+		}
+	}
+	switch (go)
+	{
+
+	case(72):
+		x--;
+		break;
+	case(80):
+		x++;
+		break;
+	case(75):
+		y--;
+		break;
+	case (77):
+		y++;
+		break;
+	case (13):
+		if (rules(x, y, arr, ship, d))
+			enteringship(tx, ty, ship, d, arr);
+		break;
+	case(32):
+		d == 1 ? d = 2 : d = 1;
+		break;
+	default:
+		break;
+	}
+	if (x >= 10)
+		x = 0;
+	if (y >= 10)
+		y = 0;
+	if (x < 0)
+		x = 9;
+	if (y < 0)
+		y = 9;
+
+	if (arr[tx][ty] <= 0)
+	{
+		arr[tx][ty] = 0;
+
+
+		for (int i = 0; i < ship; i++)
+		{
+			if (d == 1) {
+
+				if (arr[x][y + i] > 0)
+					break;
+				else
+					arr[x][y + i] = -1;
+			}
+
+			else {
+				if (arr[x + i][y] > 0)
+					break;
+				else
+					arr[x + i][y] = -1;
+			}
+		}
+	}
+
+}
+void board(int x, int y, int ship, int d, int(*arr)[10]) {
+
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			if (arr[i][j] > 0)
+				cout << "\033[32muu\033[0m";
+			else if (arr[i][j] < 0) {
+				cout << "uu";
+			}
+
+
+			else
+				cout << "[]";
+		}
+		cout << endl;
+
+	}
+
+
+
+
+
+
+}
+
+void gamebegin() {
+	int arrhumancreate[10][10] = { 0 };
+	int arrbotcreate[10][10] = { 0 };
+	int x = 0;
+	int y = 0;
+	arrhumancreate[x][y] = -1;
+	int say = 0;
+	int d = 1;
+	int battleshipsize = 1;
+	while (battleshipsize < 5)
+	{
+		int times = 5 - battleshipsize;
+		while (!findelement(battleshipsize, times, arrhumancreate)) {
+			int go = getch();
+			clearConsole();
+			cout << go << endl;
+			shipcordinates(x, y, go, battleshipsize, d, arrhumancreate);
+			board(x, y, battleshipsize, d, arrhumancreate);
+			cout << endl;
+			cout << x << ":" << y << endl << endl << endl;
+			for (int i = 0; i < 10; i++)
+			{
+				for (int j = 0; j < 10; j++)
+				{
+					cout << arrhumancreate[i][j] << " ";
+				}
+				cout << endl;
+			}
+			cout << d << endl;
+			cout << findelement(times, battleshipsize, arrhumancreate) << endl;
+
+
+		}
+		battleshipsize++;
+	}
+	botcreateboard(arrbotcreate);
+	goongame(arrhumancreate, arrbotcreate);
+}
+
+void selection(int b) {
+	switch (b)
+	{
+	case 1:
+		gamebegin();
+		break;
+	case 2:
+		gamebeginrvsr();
+		break;
+	case 3:
+		gamebeginhvsrr();
+		break;
+	default:
+		break;
+	}
+
+
+}
+
 void menu() {
 
 	int i = 0;
